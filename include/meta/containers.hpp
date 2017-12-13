cc1ef615254e70ea98609c098dc26b23fc6de852
Fletcher Blight <fletcher.blight+github@gmail.com>
2017-12-13 12:47:40 +1000

    meta:: Structural includes

---
+++ include/meta/containers.hpp
@@ -0,0 +1,6 @@
+#ifndef     _META_CONTAINERS_HPP_
+#define     _META_CONTAINERS_HPP_
+
+#include "container/string.hpp"
+
+#endif  //  _META_CONTAINERS_HPP_
---
+++ include/meta/traits.hpp
@@ -0,0 +1,6 @@
+#ifndef     _META_TRAITS_HPP_
+#define     _META_TRAITS_HPP_
+
+#include "traits/is_same_template_type.hpp"
+
+#endif  //  _META_TRAITS_HPP_
